faa8f473e2a8ada0469b9d824dc44cd0b14b9ff8
Richard Mwewa <r1chardmw3wa@gmail.com>
2021-08-11 13:58:14 +0200

    Update banner.py

--- assets/banner.py
+++ assets/banner.py
@@ -1,8 +1,8 @@
-import os
+
 from assets.colors import white,reset
 
 def banner():
-	os.system("clear")
+
 	print("""%s
 ┏━━━┓━━━━━━━━━┏┓━━━━━━━━━━━━━━━┏━━━┓┏┓━┏┓
 ┃┏━┓┃━━━━━━━━┏┛┗┓━━━━━━━━━━━━━━┃┏━┓┃┃┃━┃┃
